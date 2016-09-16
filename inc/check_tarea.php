8acee9a4af99bb179dce9f94023f987a6c9ff6ea
Odei Alba <oalbaab14dw@ikzubirimanteo.com>
2016-09-16 11:32:00 +0000

    tachar funciona

---
+++ inc/check_tarea.php
@@ -0,0 +1,35 @@
+<?php
+
+    function IsChecked($chkname,$value){
+        if(!empty($_POST[$chkname])){
+            foreach($_POST[$chkname] as $chkval){
+                if($chkval == $value){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    $dp = mysql_connect("localhost", "root", "" );
+    mysql_select_db("todo", $dp);
+    $cod_lista = $_GET['cod_lista'];
+    
+    $sql2 = "SELECT * FROM contenido WHERE cod_lista = '".$cod_lista."'";
+    $lst2 = mysql_query($sql2);
+    while ($row2 = mysql_fetch_assoc($lst2)) {
+        $cod_tarea = $row2['cod_tarea'];
+        if (IsChecked('check',$cod_tarea)){
+            //echo "$cod_lista $cod_tarea";
+            $aldatu="UPDATE contenido SET status='checked' WHERE cod_lista='".$cod_lista."' AND cod_tarea='".$cod_tarea."'";
+            mysql_query($aldatu);
+        } else {
+            //echo $cod_tarea." ".$_POST[$cod_tarea];
+            $aldatu="UPDATE contenido SET status='correcto' WHERE cod_lista='".$cod_lista."' AND cod_tarea='".$cod_tarea."'";
+            mysql_query($aldatu);
+        }
+        
+    }
+    mysql_close($dp);
+    header("Location: ../index2.php");
+    
+?>
--- index2.php
+++ index2.php
@@ -29,19 +29,25 @@
             $cod_user = $_SESSION["usuario"];
         }
         
-        $sql = "SELECT * FROM listas WHERE cod_user = '$cod_user'";
+        $sql = "SELECT * FROM listas WHERE cod_user = '$cod_user' AND status = 'correcto'";
         $lst = mysql_query($sql);
         while ($row = mysql_fetch_assoc($lst)) {
+            echo "<form  enctype='multipart/form-data' action='inc/check_tarea.php?cod_lista=".$row['cod_lista']."' method='post'>";
             echo "<table border=1>
-            <tr><th>NOMBRE</th></tr>
-            <tr><td>".$row['list_name']."</td></tr>
-            <tr><th>TAREAS</th></tr>";
-            $sql2 = "SELECT * FROM contenido WHERE cod_lista = '".$row['cod_lista']."'";
+            <tr><th colspan=3>NOMBRE</th></tr>
+            <tr><td><button>Archivar</button></td><td><button onclick=\"seguroFac($row[cod_fac]);\">Eliminar</button></td><td>".$row['list_name']."</td></tr>
+            <tr><th colspan=3>TAREAS</th></tr>";
+            $sql2 = "SELECT * FROM contenido WHERE cod_lista = '".$row['cod_lista']."' AND status = 'correcto'";
             $lst2 = mysql_query($sql2);
             while ($row2 = mysql_fetch_assoc($lst2)) {
-                echo "<tr><td>".$row2['descripcion']."</td></tr>";
+                echo "<tr><td></td><td><input type='checkbox' name='check[]' value='".$row2['cod_tarea']."'/></td><td>".$row2['descripcion']."</td></tr>";
             }
-            echo "</table><br/>";
+            $sql3 = "SELECT * FROM contenido WHERE cod_lista = '".$row['cod_lista']."' AND status = 'checked'";
+            $lst3 = mysql_query($sql3);
+            while ($row3 = mysql_fetch_assoc($lst3)) {
+                echo "<tr><td></td><td><input type='checkbox' name='check[]' value='".$row3['cod_tarea']."' checked/></td><td><s>".$row3['descripcion']."</s></td></tr>";
+            }
+            echo "<tr><td colspan=3><input type='submit' name='guardar' value='Guardar'/></td></tr></table></form><br/>";
         }
         mysql_close($dp);
     ?>
