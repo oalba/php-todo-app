4721353b9fdc3facff6272a0d69e96f652423416
Odei Alba <oalbaab14dw@ikzubirimanteo.com>
2016-09-27 06:28:47 +0000

    intentando dos array

--- inc/index2an.php
+++ inc/index2an.php
@@ -20,7 +20,7 @@
             }
             $listas[] = array('cod_lista'=> $cod_lista, 'list_name'=> $list_name, 'tareas' => $tareas);
         }
-        $sql4 = "SELECT * FROM users WHERE cod_user != '$cod_user'";
+        /*$sql4 = "SELECT * FROM users WHERE cod_user != '$cod_user'";
         $lst4 = mysql_query($sql4);
         while ($row4 = mysql_fetch_assoc($lst4)) {
             $cod_user = $row4[cod_user];
@@ -29,8 +29,10 @@
             //if ($row4[cod_user]!=$cod_user) {
                 
             //}
-        }
+        }*/
         mysql_close($dp);
         $json_string = json_encode($listas);
         echo $json_string;
+        /*$json_string2 = json_encode($users);
+        echo $json_string2;*/
 ?>
